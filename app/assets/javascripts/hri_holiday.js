946842a67e0c29252a83243ee78191fbb6da447c
CJ Avilla <cjavilla@gmail.com>
2014-11-16 14:14:28 -0800

    adding globe visualization

--- app/assets/javascripts/hri_holiday.js
+++ app/assets/javascripts/hri_holiday.js
@@ -1,10 +1,10 @@
-/*globals window, App, $, navigator */
+/*globals Detector, window, document, App, $, navigator */
 window.App = {
   Models: {},
   Collections: {},
   Views: {},
   Routers: {},
-  initialize: function() {
+  initialize: function () {
     var newWish = new App.Models.Wish();
 
     navigator.geolocation.getCurrentPosition(function (pos) {
@@ -14,16 +14,20 @@ window.App = {
       console.log(arguments);
     }, {
       enableHighAccuracy: true,
-			timeout: 10000000,
-			maxaAge: 0
+      timeout: 10000000,
+      maxaAge: 0
     });
 
-    var sendWish = new App.Views.SendWish({ model: newWish });
+    var sendWish = new App.Views.SendWish({
+      model: newWish
+    });
     $('#new-wish').append(sendWish.render().$el);
 
     App.wishes.fetch();
 
-    var treeView = new App.Views.TreeView({ collection: App.wishes });
+    var treeView = new App.Views.TreeView({
+      collection: App.wishes
+    });
     $('#main').append(treeView.render().$el);
   },
 
@@ -43,6 +47,39 @@ window.App = {
       el: document.getElementById('canvas')
     });
   },
+
+  initializeGlobe: function () {
+    if (!Detector.webgl) {
+      Detector.addGetWebGLMessage();
+    } else {
+      var container = document.getElementById('container');
+      var globe = new DAT.Globe(container);
+
+      var i, tweens = [];
+      var settime = function (globe, t) {
+        return function () {
+          new TWEEN.Tween(globe).to({
+            time: t / 3
+          }, 500).easing(TWEEN.Easing.Cubic.EaseOut).start();
+        };
+      };
+
+      TWEEN.start();
+
+      App.wishes.fetch().then(function () {
+        globe.addData(App.wishes.withMagnitude(), {
+          format: 'legend',
+          name: 'Holiday Cheer',
+          animated: true
+        });
+
+        globe.createPoints();
+        settime(globe, 0)();
+        globe.animate();
+        document.body.style.backgroundImage = 'none'; // remove loading
+      });
+    }
+  }
 };
 
 function noImage(img) {
